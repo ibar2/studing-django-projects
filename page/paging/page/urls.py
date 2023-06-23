1a8cede90dd770a1e4dc9539110b775bb34cb130
ibar <ibar.student@simad.edu.so>
2023-06-23 20:17:20 +0300

    Creating this as a sample for a project i am going wrok for future and will upload it too

---
+++ page/paging/page/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.homepage, name='home'),
+    path('login', views.loginpage, name='login'),
+    path('signup', views.signup, name='signup')
+]
---
+++ page/paging/page/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def homepage(req):
+    return render(req, 'page/home.html')
+
+
+def loginpage(req):
+    return render(req, 'page/Login.html')
+
+
+def signup(req):
+    return render(req, 'page/signup.html')
